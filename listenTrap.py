e8cc36fb5b21b9d0d371ef62a9584ee92c1ddffc
Treclo <alberto.rodriguez.santiago.dev@gmail.com>
2021-08-12 12:35:20 +0200

    Con las ips

--- listenTrap.py
+++ listenTrap.py
@@ -28,12 +28,109 @@ def cbFun(transportDispatcher, transportDomain, transportAddress, wholeMsg):
 			return
 		reqMsg, wholeMsg = decoder.decode(wholeMsg, asn1Spec=pMod.Message(),)
 		ip = '%s' % ( transportAddress[0] )
+		if ip == "10.36.34.17":
+			machine = "axpr0014"
+		elif ip == "10.66.225.132":
+			machine = "iab15051"
+		elif ip == "10.36.34.12":
+			machine = "axpr0009"
+		elif ip == "10.65.64.33" or ip == "10.65.0.190":
+			machine = "amparo"
+		elif ip == "10.65.67.152" or ip == "10.65.64.55" or ip == "10.65.64.56" or ip == "10.65.64.45":
+			machine = "berta10.berta20"
+		elif ip == "10.65.64.104" or ip == "10.65.0.232":
+			machine = "carmen"
+		elif ip == "10.65.0.186" or ip == "10.65.0.187" or ip == "10.65.64.29" or ip == "10.65.64.30":
+			machine = "cristina10.cristina20"
+		elif ip == "10.65.64.90" or ip =="10.65.0.219":
+			machine = "dalila"
+		elif ip == "10.65.64.89":
+			machine = "dunaida10"
+		elif ip == "10.65.64.34":
+			machine = "dunaida20"
+		elif ip == "10.65.67.136" or ip == "10.65.3.59":
+			machine = "edelia"
+		elif ip == "10.65.64.96" or ip == "10.65.64.96" or ip == "10.65.0.224":
+			machine = "engracia"
+		elif ip == "10.65.64.240" or ip == "10.65.1.169":
+			machine = "front3"
+		elif ip == "10.72.224.73":
+			machine = "fsb1x106"
+		elif ip == "10.72.224.26":
+			machine = "fsb1x117"
+		elif ip == "10.72.224.74":
+			machine = "fsb2x107"
+		elif ip == "10.72.224.27":
+			machine = "fsb2x617"
+		elif ip == "10.66.224.36" or ip == "10.66.224.36":
+			machine = "hub00902"
+		elif ip == "10.66.224.34" or ip == "10.66.224.34" or ip == "10.66.224.34":
+			machine = "hub00903"
+		elif ip == "10.66.227.166" or ip == "10.66.227.165":
+			machine = "hub11550.hub26550"
+		elif ip == "10.66.224.227" or ip == "10.66.224.228":
+			machine = "hub15450.hub25950"
+		elif ip == "10.66.225.173" or ip == "10.66.225.167":
+			machine = "hub15451.hub25951"
+		elif ip == "10.64.34.45":
+			machine = "ripley"
+		elif ip == "10.66.224.28" or ip == "10.66.224.24":
+			machine = "iab10050.iab20150"
+		elif ip == "10.66.224.29" or ip == "10.66.224.25":
+			machine = "iab10051.iab20151"
+		elif ip == "10.66.224.30" or ip == "10.66.224.26":
+			machine = "iab10052.iab20152"
+		elif ip == "10.66.224.81":
+			machine = "iab10355"
+		elif ip == "10.66.224.63":
+			machine = "iab20755"
+		elif ip == "10.66.224.54" or ip == "10.66.224.55":
+			machine = "iab10250.iab21050"
+		elif ip == "10.66.224.27" or ip == "10.66.224.31" or ip == "10.66.225.15":
+			machine = "iab10252.iab20352"
+		elif ip == "10.66.224.64" or ip == "10.66.224.65" or ip == "10.66.208.46" or ip == "10.66.208.47":
+			machine = "iab10751.iab20851"
+		elif ip == "10.66.224.71" or ip == "10.66.224.72":
+			machine = "iab10753.iab20853"
+		elif ip == "10.66.225.131" or ip == "10.66.225.137":
+			machine = "iab15050.iab25150"
+		elif ip == "10.66.225.145" or ip == "10.66.225.150":
+			machine = "iab15250.iab25350"
+		elif ip == "10.66.225.158" or ip == "10.66.225.116":
+			machine = "iab15750.iab25850"
+		elif ip == "10.66.225.139" or ip == "10.66.227.118":
+			machine = "iab15052.iab25152"
+		elif ip == "10.66.225.123" or ip == "10.66.225.124":
+			machine = "iab15253.iab25353"
+		elif ip == "10.66.225.11" or ip == "10.66.225.12":
+			machine = "iab15752.iab25852"
+		elif ip == "10.66.224.82":
+			machine = "iab10354"
+		elif ip == "10.66.224.83":
+			machine = "iab20754"
+		elif ip == "10.66.225.134":
+			machine = "iab15053"
+		elif ip == "10.66.225.138":
+			machine = "iab25151"
+		elif ip == "10.66.225.140":
+			machine = "iab25153"
+		elif ip == "10.36.34.252":
+			machine = "axpr0009"
+		else:
+			try:
+				machine = socket.gethostbyaddr(ip)[0]
+				machine = machine[0:machine.find('.')]
+			except:
+				machine = ip
 
-		try:
-			machine = socket.gethostbyaddr(ip)[0]
-			machine = machine[0:machine.find('.')]
-		except:
-			machine = ip
+		if machine == "alba10-neg":
+			machine = "alba10"
+		elif machine == "alba20-neg":
+			machine = "alba20"
+		elif machine == "ssb16350-cpd":
+			machine = "ssb16350"
+		elif machine == "ssb26450-cpd":
+			machine = "ssb26450"
 
 		reqPDU = pMod.apiMessage.getPDU(reqMsg)
 		if reqPDU.isSameTypeWith(pMod.TrapPDU()):
